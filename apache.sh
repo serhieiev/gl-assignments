e4bc8dcfb99105625daf24285090602945f05dc2
Max Serhieiev <serhieiev@users.noreply.github.com>
2022-12-09 16:35:45 +0200

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -9,4 +9,4 @@ sudo apt install apache2 -y
 # configuring apache
 sudo chown -R $USER:$USER /var/www/
 sudo chmod -R 775 /var/www/
-echo "<html><body><h1>Maksym Serhieiev</h1></body></html>" > echo "<html><body><h1>Maksym Serhieiev</h1></body></html>" > /var/www/html/index.html
+echo "<html><body><h1>Maksym Serhieiev</h1></body></html>" > /var/www/html/index.html
